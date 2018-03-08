764e88675b671b876d6ec0add90347b35fd6b269
Surkhojb <juanjo@ragecoders.com>
2018-03-08 12:24:08 +0100

    Added mapper for database dto

---
+++ app/src/main/java/com/clean/juanjo/newsapp/domain/mapper/ArticleDtoToArticleModelMapper.java
@@ -0,0 +1,36 @@
+package com.clean.juanjo.newsapp.domain.mapper;
+
+import com.clean.juanjo.newsapp.data.local.ArticleDto;
+import com.clean.juanjo.newsapp.domain.model.ArticleModel;
+
+/**
+ * Created by Juanjo Berenguer.
+ * Email: surkhojb@gmail.com.
+ * Date: 08/03/2018.
+ */
+
+public class ArticleDtoToArticleModelMapper implements Mapper<ArticleModel,ArticleDto> {
+  @Override public ArticleModel map(ArticleDto dto) {
+    ArticleModel model = new ArticleModel();
+    model.setTitle(dto.getTitle());
+    model.setDescription(dto.getDescription());
+    model.setDateAndAuthor(dto.getDateAndAuthor());
+    model.setSource(dto.getSource());
+    model.setUrlImage(dto.getUrlImage());
+    model.setUrlToArticle(dto.getUrlToArticle());
+
+    return model;
+  }
+
+  @Override public ArticleDto inverseMap(ArticleModel model) {
+    ArticleDto dto = new ArticleDto();
+    dto.setTitle(model.getTitle());
+    dto.setDescription(model.getDescription());
+    dto.setDateAndAuthor(model.getDateAndAuthor());
+    dto.setSource(model.getSource());
+    dto.setUrlImage(dto.getUrlImage());
+    dto.setUrlToArticle(dto.getUrlToArticle());
+
+    return dto;
+  }
+}
